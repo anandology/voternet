2ee2f69cd03e13b03efe5ebb69ea20ad08c97084
Anand Chitipothu <anandology@gmail.com>
2014-01-27 21:17:41 +0530

    added setup.py

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+import glob, os
+
+dependencies = [
+    'web.py',
+    'psycopg2'
+]
+
+setup(
+    name='voternet',
+    version='0.1.0-dev',
+    description='Voternet',
+    packages=find_packages(exclude=["ez_setup"]),
+    install_requires=dependencies.split()
+)
