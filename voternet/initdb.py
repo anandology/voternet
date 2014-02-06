90ca514d2b7ee17da1f87981974dfe182818a7a8
Anand Chitipothu <anandology@gmail.com>
2014-02-06 11:56:27 +0530

    updated initdb.py to add polling booths.

--- voternet/initdb.py
+++ voternet/initdb.py
@@ -20,9 +20,13 @@ def get_pc(code):
 def get_ac(code):
     return db.select("places", where="type='AC' AND code=$code", vars=locals())[0]
 
+@web.memoize
+def get_ward(ac_id, code):
+    return db.select("places", where="type='WARD' AND ac_id=$ac_id AND code=$code", vars=locals())[0]
+
 def add_acs(state):
     for line in open("places/ac.txt"):
-        pc, ac, name = line.strip().split(" - ", 2)
+        pc, ac, name = line.strip("\n").split(" - ", 2)
         name = ac + " - " + name
         if not db.select("places", where="type='AC' AND code=$ac", vars=locals()):
             parent = get_pc(pc)
@@ -30,17 +34,17 @@ def add_acs(state):
 
 def add_pcs(state):
     for line in open("places/pc.txt"):
-        name = line.strip()
+        name = line.strip("\n")
         code = name.split("-")[0].strip()
         if not db.select("places", where="type='PC' AND code=$code", vars=locals()):
             db.insert("places", name=name, type="PC", code=code, parent_id=state.id, state_id=state.id)
 
 def add_wards(state):
     for line in open("places/wards.txt"):
-        pc, ac, code, name = line.strip().split("\t")
+        pc, ac, code, name = line.strip("\n").split("\t")
         name = code + " - " + name
-        if not db.select("places", where="type='WARD' AND code=$code", vars=locals()):
-            parent = get_ac(ac)
+        parent = get_ac(ac)
+        if not db.select("places", where="type='WARD' AND parent_id=$parent.id AND code=$code", vars=locals()):
             ac_id = parent.id
             pc_id = get_pc(pc).id
             db.insert("places", name=name, type="WARD", code=code, 
@@ -48,6 +52,26 @@ def add_wards(state):
                 state_id=state.id,
                 pc_id=pc_id,
                 ac_id=ac_id)
+
+def add_polling_booths(state):
+    for line in open("places/polling_booths.txt"):
+        pc, ac, ward, code, name = line.strip("\n").split("\t")
+        name = code + " - " + name
+
+        pc_id = get_pc(pc).id
+        ac_id = get_ac(ac).id
+        if ward:
+            ward_id = get_ward(ac_id, ward).id
+        else:
+            ward_id = None
+
+        if not db.select("places", where="type='PB' AND ac_id=$ac_id AND code=$code", vars=locals()):
+            db.insert("places", name=name, type="PB", code=code, 
+                parent_id=ac_id,
+                state_id=state.id,
+                pc_id=pc_id,
+                ac_id=ac_id,
+                ward_id=ward_id)
 
 def main():
     if not db.select("places", where="type='STATE' AND code='karnataka'"):
@@ -57,6 +81,7 @@ def main():
     add_pcs(state)
     add_acs(state)
     add_wards(state)
-    
+    add_polling_booths(state)
+
 if __name__ == '__main__':
     main()
