8128c58e6beb819dce6cea8d050b83d59f2bf0fb
Anand Chitipothu <anandology@gmail.com>
2014-04-28 14:54:18 +0530

    API to voter search

--- voternet/voter_search.py
+++ voternet/voter_search.py
@@ -3,6 +3,7 @@ import web
 from models import Voter
 from webapp import check_config
 import logging
+import json
 
 logger = logging.getLogger(__name__)
 
@@ -16,17 +17,37 @@ render = web.template.render(path)
 
 class voterid_search:
     def GET(self):
-        i = web.input(voterid="")
+        i = web.input(voterid="", format="html")
         if i.voterid:
             logger.info("request for %s", i.voterid)
-            voter = Voter.find(i.voterid)
-            if voter:
-                logger.info("%s - match found", i.voterid)
-            else:
-                logger.info("%s - no match found", i.voterid)
+            voter = self.find_voter(i.voterid)
             return render.voter(voter, query=i.voterid)
         else:
             return render.voter()
+
+    def find_voter(self, voterid):
+        voter = Voter.find(voterid)
+        if voter:
+            logger.info("%s - match found", voterid)
+        else:
+            logger.info("%s - no match found", voterid)
+        return voter
+
+    def GET_json(self, i):
+        web.header("content-type", "application/json")        
+        if not i.voterid:
+            d = {
+                "status": "error", 
+                "errcode": "voterid-missing", 
+                "message": "Please provide voterid as query parameter."
+            }
+        else:
+            voter = self.find_voter(i.voterid)
+            if voter:
+                d = dict(voter, status="ok")
+            else:
+                d = dict(status="error", errcode="voterid-invalid", message="Invalid Voter ID")
+        return json.dumps(d)
 
 def main():
     FORMAT = "%(asctime)s [%(name)s] [%(levelname)s] %(message)s"
