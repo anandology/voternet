ce6aa0203975c0a76b278a57b0296791899e178a
Anand Chitipothu <anandology@gmail.com>
2014-04-28 14:57:08 +0530

    more API stuff

--- voternet/voter_search.py
+++ voternet/voter_search.py
@@ -18,20 +18,18 @@ render = web.template.render(path)
 class voterid_search:
     def GET(self):
         i = web.input(voterid="", format="html")
+        if i.format == "json":
+            return self.GET_json(i)
+        else:
+            return self.GET_html(i)
+
+    def GET_html(self, i):
         if i.voterid:
             logger.info("request for %s", i.voterid)
             voter = self.find_voter(i.voterid)
             return render.voter(voter, query=i.voterid)
         else:
             return render.voter()
-
-    def find_voter(self, voterid):
-        voter = Voter.find(voterid)
-        if voter:
-            logger.info("%s - match found", voterid)
-        else:
-            logger.info("%s - no match found", voterid)
-        return voter
 
     def GET_json(self, i):
         web.header("content-type", "application/json")        
@@ -47,7 +45,15 @@ class voterid_search:
                 d = dict(voter, status="ok")
             else:
                 d = dict(status="error", errcode="voterid-invalid", message="Invalid Voter ID")
-        return json.dumps(d)
+        return json.dumps(d)            
+
+    def find_voter(self, voterid):
+        voter = Voter.find(voterid)
+        if voter:
+            logger.info("%s - match found", voterid)
+        else:
+            logger.info("%s - no match found", voterid)
+        return voter
 
 def main():
     FORMAT = "%(asctime)s [%(name)s] [%(levelname)s] %(message)s"
